e47da7e04c3c3a7e6eae4eb8a5ff56d7023b2771
Evan Chisholm <elchisholm248@gmail.com>
2024-06-02 19:34:38 -0400

    wrote function to construct tree

--- c/coder.c
+++ c/coder.c
@@ -3,6 +3,8 @@
 // #include<stdbool.h>
 #include<malloc.h>
 #include<string.h>
+#include<math.h>
+#include<stdbool.h>
 
 #define bn struct BinaryNode
 
@@ -12,6 +14,66 @@ struct BinaryNode {
     char val;
     float prob;
 };
+
+int cmp(bn *a, bn *b) {
+    float diff = a->prob - b->prob;
+
+    if (diff == 0) return 0;
+
+    return diff > 0 ? ceil(diff) : floor(diff);
+}
+
+bn *constructTree(bn **initial_nodes, int n) {
+    printf("%d\n", n);
+    printf("made it here!!\n");
+    if(n == 1) {
+        return initial_nodes[0];
+    }
+
+    qsort(initial_nodes, n, sizeof(bn*), cmp);
+
+    int new_size = ceil(((float) n) / 2);
+    bn **new_nodes;
+    new_nodes = (bn **)malloc(new_size * sizeof(bn *));
+
+    int new_nodes_loc = 0;
+    for(int i = 0; i < n; i += 2) {
+        if(i == (n - 1)) {
+            new_nodes[new_nodes_loc] = initial_nodes[i];
+            continue;
+        }
+
+        bn *left = initial_nodes[i];
+        bn *right = initial_nodes[i + 1];
+
+        bn *parent = (bn *)malloc(sizeof(bn));
+
+        parent->left = left;
+        parent->right = right;
+        parent->val = 0;
+        parent->prob = left->prob + right->prob;
+
+        new_nodes[new_nodes_loc] = parent;
+
+        new_nodes_loc++;
+    }
+
+    bn *root = constructTree(new_nodes, new_size);
+
+    free(new_nodes);
+    return root;
+}
+
+int count_leaf(bn *node, int depth) {
+    if(node->left == NULL && node->right == NULL) {
+        return 1;
+    } else {
+        int out = 0;
+        if(node->left != NULL) out += count_leaf(node->left, depth + 1);
+        if(node->right != NULL) out += count_leaf(node->right, depth + 1);
+        return out;
+    }
+}
 
 int main() {
     FILE *file;
@@ -78,12 +140,22 @@ int main() {
         current_loc++;
     }
 
+    float prob_sum = 0;
 
     for(int i = 0; i < used_chars; i++) {
         printf("%c prob: %f\n", initial_nodes[i]->val, initial_nodes[i]->prob);
+        prob_sum += initial_nodes[i]->prob;
     }
 
     fclose(file);
+
+
+    bn *root = constructTree(initial_nodes, used_chars);
+    printf("made it here!!\n");
+
+    printf("%f\n", root->prob);
+    printf("%f\n", prob_sum);
+    printf("%d\n", count_leaf(root, 0));
 
     for(int i = 0; i < used_chars; i++) {
         free(initial_nodes[i]);
